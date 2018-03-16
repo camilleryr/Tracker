db2288478de91a259e261a658a281dd6b4de9b81
Camilleryr <camiller.yr@gmail.com>
2018-03-16 13:30:47 -0700

    updated api in too many ways for one commit, sorry

---
+++ ActivityTrackerAPI/Controllers/AlexaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrackerAPI.Data;
+using TrackerAPI.Models;
+
+namespace ActivityTrackerAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Alexa")]
+    public class AlexaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlexaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Alexa
+        [HttpGet]
+        public string Get()
+        {
+            Activity LatestActivity = _context.Activity.OrderBy(x => x.StartTime).Last();
+            return  $"Chris' last run was on {LatestActivity.StartTime}, was {LatestActivity.Distance} miles and took {(LatestActivity.Distance / LatestActivity.Pace) * 60} minutes";
+        }
+
+    }
+}
